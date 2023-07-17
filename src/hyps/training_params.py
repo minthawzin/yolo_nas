9a3f54c6d88226b83c124f15771466f7770e19ad
Min Thaw Zin <minthawzin@globalwalkers.co.jp>
2023-07-17 14:02:00 +0630

    [update] train parameters

--- src/hyps/training_params.py
+++ src/hyps/training_params.py
@@ -2,25 +2,28 @@ from super_gradients.training.losses import PPYoloELoss
 from super_gradients.training.metrics import DetectionMetrics_050
 from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
 
-train_params = {
-    "warmup_initial_lr": 1e-6,
-    "initial_lr": 5e-4,
-    "lr_mode": "cosine",
-    "cosine_final_lr_ratio": 0.1,
-    "optimizer": "AdamW",
-    "zero_weight_decay_on_bias_and_bn": True,
-    "lr_warmup_epochs": 3,
-    "warmup_mode": "linear_epoch_step",
-    "optimizer_params": {"weight_decay": 0.0001},
-    "ema": True,
-    "ema_params": {"decay": 0.9, "decay_type": "threshold"},
-    "max_epochs": 100,
-    "mixed_precision": True,
-    "loss": PPYoloELoss(use_static_assigner=False, num_classes=3, reg_max=16),
-    "valid_metrics_list": [
-        DetectionMetrics_050(score_thres=0.1, top_k_predictions=300, num_cls=2, normalize_targets=True,
-                             post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01,
-                                                                                    nms_top_k=1000, max_predictions=300,
-                                                                                    nms_threshold=0.7))],
+def getTrainingParams( class_names ):
+    train_params = {
+        "warmup_initial_lr": 1e-6,
+        "initial_lr": 5e-4,
+        "lr_mode": "cosine",
+        "cosine_final_lr_ratio": 0.1,
+        "optimizer": "AdamW",
+        "zero_weight_decay_on_bias_and_bn": True,
+        "lr_warmup_epochs": 3,
+        "warmup_mode": "linear_epoch_step",
+        "optimizer_params": {"weight_decay": 0.0001},
+        "ema": True,
+        "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+        "max_epochs": 100,
+        "mixed_precision": True,
+        "loss": PPYoloELoss(use_static_assigner=False, num_classes=len( class_names ), reg_max=16),
+        "valid_metrics_list": [
+            DetectionMetrics_050(score_thres=0.1, top_k_predictions=300, num_cls= len( class_names ), normalize_targets=True,
+                                post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01,
+                                                                                        nms_top_k=1000, max_predictions=300,
+                                                                                        nms_threshold=0.7))],
 
-    "metric_to_watch": 'mAP@0.50'}
+        "metric_to_watch": 'mAP@0.50'}
+    
+    return train_params
--- train.py
+++ train.py
@@ -3,21 +3,24 @@ from super_gradients.common.object_names import Models
 from super_gradients.training import models
 
 from src.dataset.dataloader import DatasetLoader
-from src.hyps.training_params import train_params
+from src.hyps.training_params import getTrainingParams
 
 #trainer = Trainer(experiment_name="yolo_nas_s_soccer_players", ckpt_root_dir="./content/sg_checkpoints_dir/")
 #net = models.get(Models.YOLO_NAS_S, num_classes=4, pretrained_weights="coco")
 
 def main():
 
-    CLASS_NAME = ['Background', 'Human']
+    CLASS_NAME = ['Human']
     # Dataset Loading #
-    dataloader = DatasetLoader( data_dir='./dataset/', class_names=CLASS_NAME, input_img=640 )
+    dataloader = DatasetLoader( data_dir='./datasets/', class_names=CLASS_NAME, input_img=640 )
     dataloader.visualiseOriginalDataset( dataloader.train_dataset )
 
     # Model Initialiser *
     trainer = Trainer(experiment_name="crowdhuman_dataset", ckpt_root_dir="./content/sg_checkpoints_dir/")
-    net     = models.get("yolo_nas_s", num_classes=len( CLASS_NAME )+1  , pretrained_weights='coco' )
+    net     = models.get("yolo_nas_s", num_classes=len( CLASS_NAME )+1 )
+
+    # train parameters
+    train_params = getTrainingParams( CLASS_NAME )
     trainer.train(model=net, training_params=train_params, train_loader=dataloader.train_dataloader, valid_loader=dataloader.valid_dataloader)
 
 if __name__ == "__main__":
